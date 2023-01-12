291628ef6b330854ef7286c976435a2fe2d36668
aitiarenia aitiarenia <aitiarenia@gmail.com>
2023-01-12 16:32:12 +0200

    upload content

---
+++ Session4/Session4/Program.cs
@@ -0,0 +1,98 @@
+﻿// Write a C# program to print Hello and your name in the same line.
+
+using Microsoft.VisualBasic;
+using System;
+using System.Runtime.CompilerServices;
+
+string hello = "Hello";
+    string name = "Renia";
+    Console.WriteLine(hello + " " + name);
+    Console.ReadLine();
+
+//Write a C# program to print the sum of two numbers and the division of those two numbers.
+int a= 6;
+int b = 3;
+int s = a + b; 
+int d = a / b;
+Console.WriteLine( s);
+Console.WriteLine(d);
+
+Console.ReadLine();
+
+//Write a C# program to print the result of the specified operations:
+int x = -1;
+int y = 5;
+int z = 6;
+int p = x + (y * z);
+Console.WriteLine( p);
+Console.ReadLine();
+
+int k = 38;
+int j = 5;
+int w = 7;
+int r = k + (j & w);
+Console.WriteLine( r);
+Console.ReadLine();
+
+int   O= 14;
+int o = -3;
+int g = 6;
+int v = O + (( o * g) / w);
+Console.WriteLine( v);  
+Console.ReadLine();
+
+int T = 2;
+int f= 13;
+double R = T + ((f / g) * g) + Math.Sqrt(w);
+Console.WriteLine( R);  
+Console.ReadLine();
+
+int q = 4;
+int i = 9;
+double e = (Math.Pow(g, q) + Math.Pow(j, w)) / (i % q);
+Console.WriteLine( e);  
+Console.ReadLine();
+
+string age = "twenty three";
+string gender = " female";
+Console.WriteLine("You are " + gender + " and look younger than " + age );
+Console.ReadLine();
+
+
+int u = 45678;
+int Y = 60;
+int H = 24;
+int V = 365;
+long M = u / Y;
+long N = M / Y;
+long E = N / H;
+long P = E / V;
+Console.WriteLine(M);
+Console.WriteLine(N);
+Console.WriteLine(E);
+Console.WriteLine(P);
+Console.ReadLine() ;
+
+
+
+TimeSpan t = TimeSpan.FromSeconds(u);
+  
+Console.WriteLine(t.TotalMinutes);
+Console.WriteLine(t.TotalHours);
+Console.WriteLine(t.TotalDays);
+Console.WriteLine(t.TotalDays/365);
+
+Console.ReadLine();
+
+
+double C = 26;
+double K = C + 273.15;
+double F = (C * 1.8) + 32;
+Console.WriteLine(K);
+Console.WriteLine(F);
+Console.ReadLine();
+
+  
+
+
+
