928b779ef97c7f76bbbb468985963cffe6d497fe
aitiarenia aitiarenia <aitiarenia@gmail.com>
2023-01-15 17:18:51 +0200

    ex

---
+++ Session5/Session5/Ex4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Session5
+{
+    public class Ex4
+    {
+        public void mularrays ()
+        {
+            int[] first_array = { 2, 4, 9, 12 };
+            int[] second_array = { 1, 3, 7, 10 };
+            int[] third_array = new int [16];
+            int counter = 0;
+           for ( int i=0; i< first_array.Length; i++ ) 
+            {
+                
+                for (int j = 0; j < second_array.Length; j++)
+                {
+                    third_array[counter] = first_array[i] * second_array[j];
+                    Console.Write(third_array[counter]);
+                    counter++;
+
+                }
+            }
+           
+
+        }
+
+    }
+}
