04fc68ffe6d6a6bb62ffbc63fdc66e4da056e0c5
aitiarenia aitiarenia <aitiarenia@gmail.com>
2023-01-15 17:55:46 +0200

    upload

---
+++ Session-05/Session-05/Exe5.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_05
+{
+    internal class Exe5
+{
+    public void arraysort()
+    {
+
+        int[] intArray = { 0, -2, 1, 20, -31, 50, -4, 17, 89, 100 };
+        Array.Sort(intArray);
+
+    }
+}
+}
---
+++ Session-05/Session-05/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Session_05;
+using System.Text;
+
+Exe1 reversedname = new Exe1();
+reversedname.Reversestring();
+
+Exe2 klo = new Exe2();
+int s = klo.sumproduct();
+Console.WriteLine(s);
+Console.ReadLine();
+
+Exe4 mularrays = new Exe4();
+mularrays.mularrays();
+Console.WriteLine("\n");
+
+Exe5 arraysorting = new Exe5();
+arraysorting.arraysort();
+Console.ReadLine();
